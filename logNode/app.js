dd7c9034fca273983aa8ad7c76a365bfc5e592f0
rockdroid <rock.droid8@gmail.com>
2021-02-04 18:17:23 -0600

    Update app.js

--- logNode/app.js
+++ logNode/app.js
@@ -4,32 +4,32 @@ const parser = require('ua-parser-js');
 const geolite2 = require('geolite2');
 const maxmind = require('maxmind');
 const ObjectsToCsv = require('node-create-csv'); 
-const file = 'gobankingrates.com.access.log'
+const file = 'gobankingrates.com.access.log';
 const headers = ['IP', '1', '2', 'Date', 'timeZone', 'Asset', 
-                'ServerResponse', 'Size', 'URI', 'userAgent']
-var lookupCity = maxmind.open(geolite2.paths.city)
-var data = []
+                'ServerResponse', 'Size', 'URI', 'userAgent'];
+var lookupCity = maxmind.open(geolite2.paths.city);
+var data = [];
     fs.createReadStream(file)
     .pipe(csv({ headers: headers, separator: ' ' }))
     .on('data', (row) => {
-        userAgent = parser(row.userAgent)
-        row.browser = userAgent.browser.name
-        row.deviceType = userAgent.device.type
+        userAgent = parser(row.userAgent);
+        row.browser = userAgent.browser.name;
+        row.deviceType = userAgent.device.type;
         if (lookupCity.get(row.IP) != null ) {
             if (typeof lookupCity.get(row.IP).city !== 'undefined') {
-                row.city = lookupCity.get(row.IP).city.names.en
+                row.city = lookupCity.get(row.IP).city.names.en;
             } else {
-                row.city = undefined
+                row.city = undefined;
             }
             if (typeof lookupCity.get(row.IP).country !== 'undefined') {
-                row.country = lookupCity.get(row.IP).country.names.en
+                row.country = lookupCity.get(row.IP).country.names.en;
             } else {
-                row.country = lookupCity.get(row.IP).continent.names.en
+                row.country = lookupCity.get(row.IP).continent.names.en;
             }
         } else {
-            row.country = undefined
+            row.country = undefined;
         }
-        data.push(row)
+        data.push(row);
     }, this)
     .on('end', () => {
         let otc = new ObjectsToCsv(data);
